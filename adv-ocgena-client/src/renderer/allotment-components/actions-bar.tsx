48a7d96171093b6061675504442a414235ec48b3
MisterPotz <aleksgornostaev@gmail.com>
2023-06-04 14:44:55 +0300

    - setup more clean action bar buttons with tailwind css usage - remove scale animation for buttons (use color fading instead)

--- adv-ocgena-client/src/renderer/allotment-components/actions-bar.tsx
+++ adv-ocgena-client/src/renderer/allotment-components/actions-bar.tsx
@@ -1,6 +1,36 @@
 import styles from './actions-bar.module.css'
 import "@vscode/codicons/dist/codicon.css";
 
+import * as React from 'react';
+
+export interface ActionButtonProps {
+    onClick: () => void;
+    text: string;
+    iconClass: string;
+    buttonStyle?: string;
+}
+
+export function ActionButton(props: ActionButtonProps) {
+    return (
+        <button onClick={props.onClick} className={`
+            bg-transparent
+            relative
+            flex-row 
+            flex
+            px-2
+            py-2
+            rounded-sm
+            transition-colors
+            duration-300
+            ease-in-out
+            ${props.buttonStyle ? props.buttonStyle : ""} `}>
+            <div className={`relative text-sm codicon ${props.iconClass} !top-0.5`}></div>
+            <div className={`ps-1 text-gray-200 relative text-sm pe-1`}>{props.text}</div>
+        </button>
+
+    );
+}
+
 export type ActionBarProps = {
     startButtonMode: "executing" | "start";
     pauseButtonEnabled: boolean,
@@ -19,51 +49,29 @@ export function ActionBar(
     }: ActionBarProps
 ) {
     let showOutline = startButtonMode == "executing"
-
     return (
         <div className={`${styles.actionBar} h-full flex`} >
 
-            <button onClick={onClickStart} className={`
-                ${showOutline ? "outline-gree-500 outline-2 outline" : ""}
-                bg-zinc-700
-                relative
-                flex-row
-                flex    
-                px-2 py-2
-                text-green-500
-                rounded-sm
-                active:bg-green-800`}>
-                <div className={`codicon codicon-debug-start relative text-sm ${styles.myCodeIcon}`}></div>
-                <div className='ps-1 text-gray-200 relative text-sm pe-1'>Start</div>
-            </button>
+            <ActionButton
+                onClick={onClickStart}
+                iconClass='codicon-debug-start'
+                text='Start'
+                buttonStyle={`text-green-500 hover:bg-green-800 ${showOutline ? "outline-green-500 outline-2 outline" : ""}`}
+            />
 
-            <button onClick={onClickRefresh} className={`
-                bg-zinc-700
-                relative
-                flex-row
-                flex    
-                px-2 py-2
-                text-yellow-400
-                ms-2
-                rounded-sm
-                active:bg-yellow-700`}>
-                <div className={`codicon codicon-debug-restart relative text-sm ${styles.myCodeIcon}`}></div>
-                <div className='ps-1 text-gray-200 relative text-sm pe-1'>Reload</div>
-            </button>
+            <ActionButton
+                onClick={onClickRefresh}
+                iconClass='codicon-debug-restart'
+                text='Restart'
+                buttonStyle={`text-yellow-400 hover:bg-yellow-700`}
+            />
 
-            <button onClick={onOpenNewFile} className={`
-                bg-zinc-700
-                relative
-                flex-row
-                flex    
-                px-2 py-2
-                text-gray-200
-                ms-2
-                rounded-sm
-                active:bg-zinc-400`}>
-                <div className={`codicon codicon-symbol-file relative text-sm ${styles.myCodeIcon}`}></div>
-                <div className='ps-1 text-gray-200 relative text-sm pe-1'>Open file</div>
-            </button>
+            <ActionButton
+                onClick={onClickRefresh}
+                iconClass='codicon-symbol-file'
+                text='Open file'
+                buttonStyle={`text-gray-200 hover:bg-zinc-400`}
+            />
         </div>
     )
 }
