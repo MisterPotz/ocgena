db8915b07ebc7ccf94f64eef5ada4d6fd0a320d9
Alexander Gornostaev <aleksgornostaev@gmail.com>
2024-04-13 16:29:04 +0300

    trying to create normal looking context menu

--- ocgena-canvas-redux/src/features/editor/Editor.tsx
+++ ocgena-canvas-redux/src/features/editor/Editor.tsx
@@ -1,23 +1,17 @@
-import { KonvaEventObject, NodeConfig } from "konva/lib/Node"
-import { MutableRefObject, useEffect, useRef, useState } from "react"
-import { Circle, Layer, Rect, Stage } from "react-konva"
+import { KonvaEventObject } from "konva/lib/Node"
+import { useEffect, useRef, useState } from "react"
+import { Layer, Rect, Stage } from "react-konva"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import {
   elementSelected,
-  Element,
   elementSelector,
-  elementDragEpicTrigger,
   PositionUpdatePayload,
   elementDragEndEpicTrigger,
-  RectangleShape,
-  CircleShape,
   elementSelectionCancelled,
 } from "./editorSlice"
-import { CircleConfig } from "konva/lib/shapes/Circle"
-import { RectConfig } from "konva/lib/shapes/Rect"
 import { TextShape } from "./TextShape"
 import Konva from "konva"
-import { click } from "@testing-library/user-event/dist/cjs/convenience/click.js"
+import styles from "./Editor.module.css"
 
 function dragEventToPayload(
   e: KonvaEventObject<DragEvent>,
@@ -41,17 +35,39 @@ export function Editor() {
   const elements = useAppSelector(elementSelector)
   const stageRef = useRef<Konva.Stage | null>(null)
   const [selector, setSelector] = useState<SelectorOrNull>(null)
+  const elementsLayerRef = useRef<Konva.Layer | null>(null)
   const selectorDataRef = useRef<SelectorOrNull>(null)
   const selectorShapeRef = useRef<Konva.Rect | null>(null)
   const selectionLayer = useRef<Konva.Layer | null>(null)
   // State for storing elements
   const [tool, setTool] = useState<Tool | null>(null) // Current selected tool
+  const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 })
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleRightClick = (event: Konva.KonvaEventObject<PointerEvent>) => {
+    // event.preventDefault()
+    event.target.preventDefault()
+    event.evt.preventDefault()
+    const targetShapeId = event.target.id()
+    if (targetShapeId) {
+      console.log("event target", event.target.id())
+      setShowMenu(true)
+      setMenuPosition({ x: event.evt.pageX, y: event.evt.pageY })
+      dispatch(elementSelected(targetShapeId))
+    }
+  }
 
   useEffect(() => {
     const documentRightClickCallback = (ev: KeyboardEvent) => {
       console.log("key pressed", ev.key)
     }
     document.body.addEventListener("keypress", documentRightClickCallback)
+    
+    // document.addEventListener('click', function(event) {
+    //   const contextMenu = document.querySelector('.custom-context-menu');
+    //   contextMenu.style.display = 'none';
+    // });
+
     const windowMouseMoveCallback = windowMouseMoveCallbackCreator(
       selectorShapeRef,
       stageRef,
@@ -82,42 +98,62 @@ export function Editor() {
     <>
       <ToolPane onSelectTool={setTool} />
       <Stage
+        onContextMenu={handleRightClick}
         ref={stageRef}
         width={window.innerWidth}
         height={window.innerHeight}
         onMouseDown={(e: Konva.KonvaEventObject<MouseEvent>) => {
-          console.log("stage mouse down target", e.target)
+          console.log(
+            "stage mouse down target",
+            e.target,
+            "left click",
+            e.evt.button,
+          )
           const stage = stageRef.current!
-          // Ignore clicks on shapes
-          if (e.target !== stageRef.current) {
-            return
-          }
-          dispatch(elementSelectionCancelled())
-          const startX = stage.getRelativePointerPosition()?.x
-          const startY = stage.getRelativePointerPosition()?.y
+          switch (e.evt.button) {
+            case 0:
+              setShowMenu(false)
 
-          if (startX && startY) {
-            console.log("setting selector at", startX, startY)
-            const selectorData = { x: startX, y: startY }
-            selectorDataRef.current = selectorData
-            setSelector(selectorData)
+              if (e.target !== stageRef.current) {
+                return
+              }
+              dispatch(elementSelectionCancelled())
+              const startX = stage.getRelativePointerPosition()?.x
+              const startY = stage.getRelativePointerPosition()?.y
+
+              if (startX && startY) {
+                console.log("setting selector at", startX, startY)
+                const selectorData = { x: startX, y: startY }
+                selectorDataRef.current = selectorData
+                setSelector(selectorData)
+              }
+              break
+            case 2: // right click
+              break;
+            default:
+              break;
           }
         }}
       >
         <Layer
+          ref={elementsLayerRef}
+          onDragStart={e => {
+            const targetShapeID = e.target.id()
+            if (targetShapeID) {
+              dispatch(elementSelected(targetShapeID))
+            }
+          }}
           onDragEnd={e => {
             dispatch(elementDragEndEpicTrigger(dragEventToPayload(e)))
           }}
           onClick={(e: Konva.KonvaEventObject<MouseEvent>) => {
             const clickedId = e.target?.id()
             console.log("clicked ", clickedId)
+            setShowMenu(false)
             if (clickedId) {
               dispatch(elementSelected(clickedId))
             }
           }}
-          // onMouseUp={(e: Konva.KonvaEventObject<MouseEvent>) => {
-          //   console.log("onMouseUp ", e.)
-          // }}
         >
           {elements.map((el, index) => {
             return <TextShape key={el.id} {...el} />
@@ -140,6 +176,18 @@ export function Editor() {
           )}
         </Layer>
       </Stage>
+      {showMenu && (
+        <div className=".customContextMenu">
+          Custom Context Menu
+          <button
+            onClick={() => {
+              setShowMenu(false)
+            }}
+          >
+            Close Menu
+          </button>
+        </div>
+      )}
     </>
   )
 }
