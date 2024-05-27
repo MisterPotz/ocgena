211bba7fb0d7f6ecdd4952371a17d0103e4c75e8
Alexander Gornostaev <aleksgornostaev@gmail.com>
2024-05-27 09:57:42 +0300

    Created folder for electron-forge based client (v2)

---
+++ ocgena-client-v2/src/app.tsx
@@ -0,0 +1,23 @@
+import { Allotment } from "allotment";
+import { createRoot } from "react-dom/client";
+import "allotment/dist/style.css";
+import styles from './app.module.css';
+import "allotment/dist/style.css";
+
+const root = createRoot(document.body);
+
+export const App = () => (
+  <div className={styles.container}>
+    <h2>Hello from React!</h2>
+    <Allotment >
+      <Allotment.Pane minSize={200}>
+        <div>Pane 1</div>
+      </Allotment.Pane>
+      <Allotment.Pane snap>
+        <div>Pane 1</div>
+      </Allotment.Pane>
+    </Allotment>
+  </div>
+);
+
+root.render(App());
--- ocgena-client-v2/src/renderer.ts
+++ ocgena-client-v2/src/renderer.ts
@@ -27,5 +27,6 @@
  */
 
 import './index.css';
+import './app'
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
